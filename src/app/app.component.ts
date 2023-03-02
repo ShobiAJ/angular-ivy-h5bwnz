c860aa6752c95d9b26a5ba087469e7f53b60e4b3
ShobiAJ <117976538+ShobiAJ@users.noreply.github.com>
2023-03-02 20:33:30 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,33 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   countryNames = [
-    {country:'India'}, {country:'Japan'}, {country:'China'},{country:'UAE'}
-  ]
+    { country: 'India' },
+    { country: 'Japan' },
+    { country: 'China' },
+    { country: 'UAE' },
+  ];
 
-  education = [{name : "SSLC", id: 1}, {name : "MBA", id: 2},{name :  "HSC", id: 3}]
+  education = [
+    { name: 'SSLC', id: 1 },
+    { name: 'MBA', id: 2 },
+    { name: 'HSC', id: 3 },
+  ];
 
-  onSubmit(value) {
-    console.log('hi');
-    console.log(value);
+  onSubmit(form) {
+    console.log(form);
+    if (form.valid) {
+      alert('Submitted!');
+    } else {
+      alert('Invalif form!');
+    }
+    this.onReset(form);
   }
 
   onReset(form: NgForm) {
-    form.reset()
+    form.reset();
   }
 }
